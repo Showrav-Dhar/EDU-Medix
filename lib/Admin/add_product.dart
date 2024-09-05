d71ad5e89927d5c63026d49cc3e0900eb60906d8
showrav dhar <dhardip349900@gmail.com>
2024-09-05 17:52:47 +0600

    firestore problem

--- lib/Admin/add_product.dart
+++ lib/Admin/add_product.dart
@@ -1,5 +1,12 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:edu_medix_app/widget/support_widget.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:random_string/random_string.dart';
 
 class AddProduct extends StatefulWidget {
   const AddProduct({super.key});
@@ -9,6 +16,27 @@ class AddProduct extends StatefulWidget {
 }
 
 class _AddProductState extends State<AddProduct> {
+
+final ImagePicker _picker = ImagePicker();
+File?  selectedImage;
+TextEditingController namecontroller = new TextEditingController();
+
+Future getImage()async{  
+  var image = await _picker.pickImage(source: ImageSource.gallery);
+  selectedImage = File(image!.path);
+  setState(() {
+    
+  });
+}
+
+uploadItem(){
+  if(selectedImage!=null && namecontroller.text!=""){
+    String addId = randomAlphaNumeric(10);
+    Reference firebase_storageref = FirebaseStorage.instance.ref().child("blogImage").child(addId);
+  }
+}
+
+
   String? value;
 
   final List<String> categoryitem = [
@@ -64,6 +92,7 @@ class _AddProductState extends State<AddProduct> {
                   color: Color.fromARGB(255, 168, 225, 237),
                   borderRadius: BorderRadius.circular(20)),
               child: TextField(
+                controller: namecontroller,
                 decoration: InputDecoration(border: InputBorder.none),
               ),
             ),
