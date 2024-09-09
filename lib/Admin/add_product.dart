7c302ecf4e32cc3e17e71e742d5083cd0343dafd
op <mdopi35@gmail.com>
2024-09-09 22:33:24 +0600

    need firebase work from 12 min

--- lib/Admin/add_product.dart
+++ lib/Admin/add_product.dart
@@ -20,6 +20,8 @@ class _AddProductState extends State<AddProduct> {
   final ImagePicker _picker = ImagePicker();
   File? selectedImage;
   TextEditingController namecontroller = new TextEditingController();
+  TextEditingController pricecontroller = new TextEditingController();
+  TextEditingController detailcontroller = new TextEditingController();
 
   Future getImage() async {
     var image = await _picker.pickImage(source: ImageSource.gallery);
@@ -39,6 +41,8 @@ class _AddProductState extends State<AddProduct> {
       Map<String, dynamic> addProduct = {
         "Name": namecontroller.text,
         "Image": dowloadUrl,
+        "Price": pricecontroller.text,
+        "Detail": detailcontroller.text,
       };
       await DatabaseMethods().addProduct(addProduct, value!).then((value) {
         selectedImage = null;
@@ -77,122 +81,156 @@ class _AddProductState extends State<AddProduct> {
           style: AppWidget.semiboldTextFieldStyle(),
         ),
       ),
-      body: Container(
-        margin: EdgeInsets.only(left: 20.0, top: 20.0, right: 20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Upload The Product Image",
-                style: AppWidget.semiboldTextFieldStyle()),
-            SizedBox(
-              height: 20.0,
-            ),
-            selectedImage == null
-                ? GestureDetector(
-                    onTap: () {
-                      getImage();
-                    },
-                    child: Center(
-                      child: Container(
-                        height: 150,
-                        width: 150,
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black, width: 2.5),
-                            borderRadius: BorderRadius.circular(20)),
-                        child: Icon(Icons.camera_alt),
+      body: SingleChildScrollView(
+        child: Container(
+          margin:
+              EdgeInsets.only(left: 20.0, top: 20.0, right: 20.0, bottom: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Upload The Product Image",
+                  style: AppWidget.semiboldTextFieldStyle()),
+              SizedBox(
+                height: 20.0,
+              ),
+              selectedImage == null
+                  ? GestureDetector(
+                      onTap: () {
+                        getImage();
+                      },
+                      child: Center(
+                        child: Container(
+                          height: 150,
+                          width: 150,
+                          decoration: BoxDecoration(
+                              border:
+                                  Border.all(color: Colors.black, width: 2.5),
+                              borderRadius: BorderRadius.circular(20)),
+                          child: Icon(Icons.camera_alt),
+                        ),
                       ),
-                    ),
-                  )
-                : Center(
-                  child: Material(
-                      elevation: 4.0,
-                      borderRadius: BorderRadius.circular(20),
-                      child: Container(
-                        height: 150,
-                        width: 150,
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black, width: 2.5),
-                            borderRadius: BorderRadius.circular(20)),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Image.file(
-                            selectedImage!,
-                            fit: BoxFit.cover,
+                    )
+                  : Center(
+                      child: Material(
+                        elevation: 4.0,
+                        borderRadius: BorderRadius.circular(20),
+                        child: Container(
+                          height: 150,
+                          width: 150,
+                          decoration: BoxDecoration(
+                              border:
+                                  Border.all(color: Colors.black, width: 2.5),
+                              borderRadius: BorderRadius.circular(20)),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.file(
+                              selectedImage!,
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                ),
-            SizedBox(height: 20.0),
-            Text("Upload The Product Name",
-                style: AppWidget.semiboldTextFieldStyle()),
-            SizedBox(height: 20.0),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20.0),
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 168, 225, 237),
-                  borderRadius: BorderRadius.circular(20)),
-              child: TextField(
-                controller: namecontroller,
-                decoration: InputDecoration(border: InputBorder.none),
-              ),
-            ),
-            SizedBox(
-              height: 40.0,
-            ),
-            Text(
-              "Product Catagory",
-              style: AppWidget.semiboldTextFieldStyle(),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Container(
-                padding: EdgeInsets.symmetric(horizontal: 10.0),
+              SizedBox(height: 20.0),
+              Text("Upload The Product Name",
+                  style: AppWidget.semiboldTextFieldStyle()),
+              SizedBox(height: 20.0),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
                 width: MediaQuery.of(context).size.width,
                 decoration: BoxDecoration(
                     color: Color.fromARGB(255, 168, 225, 237),
                     borderRadius: BorderRadius.circular(20)),
-                child: DropdownButtonHideUnderline(
-                  child: DropdownButton<String>(
-                    items: categoryitem
-                        .map((item) => DropdownMenuItem(
-                            value: item,
-                            child: Text(
-                              item,
-                              style: AppWidget.semiboldTextFieldStyle(),
-                            )))
-                        .toList(),
-                    onChanged: ((value) => setState(() {
-                          this.value = value;
-                        })),
-                    dropdownColor: Colors.white,
-                    hint: Text(
-                      "Select Catagory",
-                      style: AppWidget.lightTextFieldStyle(),
+                child: TextField(
+                  controller: namecontroller,
+                  decoration: InputDecoration(border: InputBorder.none),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Text("Product Price", style: AppWidget.semiboldTextFieldStyle()),
+              SizedBox(height: 20.0),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 168, 225, 237),
+                    borderRadius: BorderRadius.circular(20)),
+                child: TextField(
+                  controller: pricecontroller,
+                  decoration: InputDecoration(border: InputBorder.none),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Text("Product Detail", style: AppWidget.semiboldTextFieldStyle()),
+              SizedBox(height: 20.0),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 168, 225, 237),
+                    borderRadius: BorderRadius.circular(20)),
+                child: TextField(
+                  maxLines: 6,
+                  controller: namecontroller,
+                  decoration: InputDecoration(border: InputBorder.none),
+                ),
+              ),
+              SizedBox(
+                height: 40.0,
+              ),
+              Text(
+                "Product Catagory",
+                style: AppWidget.semiboldTextFieldStyle(),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 168, 225, 237),
+                      borderRadius: BorderRadius.circular(20)),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      items: categoryitem
+                          .map((item) => DropdownMenuItem(
+                              value: item,
+                              child: Text(
+                                item,
+                                style: AppWidget.semiboldTextFieldStyle(),
+                              )))
+                          .toList(),
+                      onChanged: ((value) => setState(() {
+                            this.value = value;
+                          })),
+                      dropdownColor: Colors.white,
+                      hint: Text(
+                        "Select Catagory",
+                        style: AppWidget.lightTextFieldStyle(),
+                      ),
+                      iconSize: 35,
+                      icon: Icon(
+                        Icons.arrow_drop_down,
+                        color: Colors.black,
+                      ),
+                      value: value,
                     ),
-                    iconSize: 35,
-                    icon: Icon(
-                      Icons.arrow_drop_down,
-                      color: Colors.black,
-                    ),
-                    value: value,
-                  ),
-                )),
-            SizedBox(
-              height: 30.0,
-            ),
-            Center(
-                child: ElevatedButton(
-                    onPressed: () {
-                      uploadItem();
-                    },
-                    child: Text(
-                      "Add Product",
-                      style: TextStyle(fontSize: 20.0),
-                    ))),
-          ],
+                  )),
+              SizedBox(
+                height: 30.0,
+              ),
+              Center(
+                  child: ElevatedButton(
+                      onPressed: () {
+                        uploadItem();
+                      },
+                      child: Text(
+                        "Add Product",
+                        style: TextStyle(fontSize: 20.0),
+                      ))),
+            ],
+          ),
         ),
       ),
     );
